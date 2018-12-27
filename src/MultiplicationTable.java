2703572efbe863b6a6b5596c51443fa51f6a5f4c
ProblemSPb <ussr83@mail.ru>
2018-12-27 19:25:02 +0100

    Printing out the multiplication table, 2D array

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -6,9 +6,12 @@ public class MultiplicationTable {
 
         int [][] twoDArray = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},};
 
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.print((i * j) + " ");
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+
+                int m1 = twoDArray[0][i];
+                int m2 = twoDArray[1][j];
+                System.out.print((m1 * m2) + " ");
             }
             System.out.println();
         }
