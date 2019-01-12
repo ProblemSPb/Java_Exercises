e603a7cced6602960480648e171fd11b4107a055
ProblemSPb <ussr83@mail.ru>
2019-01-12 18:45:08 +0100

    factorial + recursion

--- src/Java_Exercises/Factorial.java
+++ src/Java_Exercises/Factorial.java
@@ -2,17 +2,27 @@
 
 package Java_Exercises;
 
-public class Factorial {
+class Calculation
+{
+    int fact(int n)
+    {
+        int result;
 
+        if(n==1)
+            return 1;
 
-        // recursion
-        int fact(int n) {
-            int result;
+        result = fact(n-1) * n;
+        return result;
+    }
+}
 
-            if (n == 1)
-                return 1;
-            result = fact(n - 1) * n;
-            return result;
-        }
+public class Factorial
+{
+    public static void main(String args[])
+    {
+        Calculation obj_one = new Calculation();
 
+        int a = obj_one.fact(4);
+        System.out.println("The factorial of the number is : " + a);
+    }
 }
